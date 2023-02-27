ed1459183a2846dbdcdaa3a2b5a0a5b5b4c46b11
Daniel Gran <contact@grandaniel.com>
2023-02-27 12:38:36 +0000

    Added full entity

--- src/arma3pbo/pbo/business/entity/header_entry.py
+++ src/arma3pbo/pbo/business/entity/header_entry.py
@@ -1,2 +1,8 @@
 class HeaderEntry:
-    pass
+    def __init__(self, filename="", mimetype=0, original_size=0, reserved=0, timestamp=0, data_size=0):
+        self.filename = filename
+        self.mimetype = mimetype
+        self.original_size = original_size
+        self.reserved = reserved
+        self.timestamp = timestamp
+        self.data_size = data_size
--- test/business/entity/test_entity.py
+++ test/business/entity/test_entity.py
@@ -6,3 +6,12 @@ class TestEntity(unittest.TestCase):
     def test_if_HeaderEntry_is_there(self):
         entity = HeaderEntry()
         assert entity
+
+    def test_meta(self):
+        entity = HeaderEntry()
+        assert entity.filename == ""
+        assert entity.mimetype == 0
+        assert entity.original_size == 0
+        assert entity.reserved == 0
+        assert entity.timestamp == 0
+        assert entity.data_size == 0
