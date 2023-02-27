d95ae2dd97cb025e6f2f34acc0dfa62e1239892a
Daniel Gran <contact@grandaniel.com>
2023-02-27 10:45:54 +0100

    Added pbo

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main(name):
+    parser = argparse.ArgumentParser(
+        prog='arma3pbo',
+        description='What the program does',
+        epilog='Text at the bottom of help')
 
+    parser.add_argument('option')  # positional argument
+    parser.add_argument('-c', '--count')  # option that takes a value
+    args = parser.parse_args()
 
-# Press the green button in the gutter to run the script.
+    a = 3;
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main('PyCharm')
