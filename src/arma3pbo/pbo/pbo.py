4c10d57e9bfd6bc54ff94eb027eeed971064b341
Daniel Gran <contact@grandaniel.com>
2023-03-03 14:16:00 +0100

    Working version

--- src/arma3pbo/pbo/pbo.py
+++ src/arma3pbo/pbo/pbo.py
@@ -10,15 +10,16 @@ class PBO:
         bytes.extend(PBOHead.head_start_bytecode)
         bytes.extend(b"\0")
 
-        for key, value in PBOHead.get_prefixes(path).items():
-            bytes.extend(PBOHead.create_header_bytecode(key, value))
+        if len(PBOHead.get_prefixes(path)) > 0:
+            for key, value in PBOHead.get_prefixes(path).items():
+                bytes.extend(PBOHead.create_header_bytecode(key, value))
+            bytes.extend(PBOHead.head_empty_entry)
 
         mission_files = tools.get_files_from_parent_path(path)
 
         for file in mission_files:
             bytes += PBOHead.create_bytecode_from_entry(PBOHead.header_entry_from_file(path, file))
         bytes.extend(PBOHead.head_empty_entry)
-        bytes.extend(b"\0")
 
         for file in mission_files:
             with open(path + "/" + file, "rb") as f:
